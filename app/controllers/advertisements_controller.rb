e9289966620262ed28624f335b1cb0f6dab7eb3e
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-02-20 13:25:43 +0300

    Changed controller Advertisement to Advertisements

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,4 +1,4 @@
-class AdvertisementController < ApplicationController
+class AdvertisementsController < ApplicationController
   def index
     @advertisements = Advertisement.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :posts, :advertisement
+  resources :posts, :advertisements
 
   get 'about' => 'welcome#about'
 
