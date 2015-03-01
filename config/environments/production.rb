b680f3a01f540f64bbe86a0ef6cb1514202394a3
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-03-01 12:09:28 +0300

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # https://zein-bloccit.herokuapp.com/
+  config.action_mailer.default_url_options = { host: 'https://zein-bloccit.herokuapp.com/'}
+
 end
