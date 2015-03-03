3fdec531cc05746cef4b637264d4056b584623d4
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-03-03 13:24:06 +0300

    Finished upload-images-checkpoint

--- Gemfile
+++ Gemfile
@@ -21,6 +21,9 @@ gem 'faker'
 gem 'devise'
 gem 'pundit'
 gem 'redcarpet'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'fog'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227202525) do
+ActiveRecord::Schema.define(version: 20150302174441) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20150227202525) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
