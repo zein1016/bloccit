8b54764285a1b5d86eb3c29a8a49651de5dfdf6d
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-03-22 11:59:51 +0400

    fixed vote spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -6,10 +6,10 @@ descrive Vote do
           expect(up_vote.valid?).to eq(true)
 
           down_vote = Vote.new(value: -1)
-          expect(down_vote).to eq(true)
+          expect(down_vote.valid?).to eq(true)
 
           invalid_vote = Vote.new(value: 2)
-          expect(invalid_vote).to eq(false)
+          expect(invalid_vote.valid?).to eq(false)
 
       end
     end
