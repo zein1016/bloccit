e2646fa5a9f73fc17591c2b90fc1b7092893a25f
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-04-07 20:55:07 +0300

    updated assignemnt

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe User do 
 
   include TestFactories 
+
   describe "#favorited(post)" do 
 
     before do 
@@ -10,19 +11,19 @@ describe User do
       @post = associated_post
       @post_2 = Post.new
       @user = authenticated_user
-     
     end
 
     it "returns `nil` if the user has not favortied the post" do 
       expect(@user.favorites.first).to eq(nil)
     end
     it "returns the appropriate favorite if it exists" do
-      favorite = Favorite.new(post: @post, user: @user)
-      expect(@user.favorited(@post)).to eq(@user.favorites.where(post: @post.id).first)
+      favorite = Favorite.create!(post: @post, user: @user)
+      expect(@user.favorited(@post)).to eq(favorite)
     end
     it "returns `nil` if the user has favorited another post" do
-      favorite = Favorite.new(post: @post_2, user: @user)
-      expect(@user.favorites.where(post: @post.id).first).to eq(nil)
+      favorite = Favorite.create!(post: @post_2, user: @user)
+      expect(@user.favorited(@post_2)).to eq(favorite)
     end
   end
 end
+
