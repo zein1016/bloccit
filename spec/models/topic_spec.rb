426d7330cef0f9fe0b436801f40b579252e77ee4
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-04-07 19:47:39 +0300

    Updated the assignment

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -22,12 +22,12 @@ describe Topic do
      describe "visible_to(user)" do
        it "returns all topics if the user is present" do
          user = true # sneaky solution; we don't need a real user, just something truthy
-         expect(Topic.visible_to(user)).to eq(Topic.all)
+         expect(Topic.visible_to(user)).to eq([@public_topic, @private_topic ])
        end
  
        it "returns only public topics if user is nil" do
          user = nil
-         expect(Topic.visible_to(user)).to eq(Topic.publicly_viewable)
+         expect(Topic.visible_to(user)).to eq([@public_topic])
        end
      end
    end
