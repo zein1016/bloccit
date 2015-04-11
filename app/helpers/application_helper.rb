bb675a42151d5eb0e83216fac9463e4156a13703
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-04-11 20:23:11 +0300

    Complete another interlude assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,5 +14,14 @@ module ApplicationHelper
   redcarpet = Redcarpet::Markdown.new(renderer, extensions)
   (redcarpet.render markdown).html_safe
 end
+ 
+
+  def up_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+  end
+
+  def down_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+  end
 
 end
