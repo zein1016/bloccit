487bfddd5b62b95a41984c512bf9ba0232a24a32
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-04-12 19:48:43 +0300

    WIP public-profiles-assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 describe "Visiting profiles" do 
-
+  
   include TestFactories
-
   before do 
     @user = authenticated_user
     @post = associated_post(user: @user)
     @comment = Comment.new(user: @user, post: @post, body:"A comment")
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
   end
 
   describe "not signed in" do
@@ -23,5 +25,17 @@ describe "Visiting profiles" do
       expect(page).to have_content(@comment.body)
     end
 
+  end
+
+  describe "user visting own profile" do 
+
+  it "shows profile" do 
+    visit user_path(current_user)
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_content(user.name)
+    expect(page).to have_content(@post.title)
+    expect(page).to have_content(@comment.body)
+    end
   end
 end
--- spec/support/test_factories.rb
+++ spec/support/test_factories.rb
@@ -1,5 +1,6 @@
 module TestFactories 
-
+  include Warden::Test::Helpers
+  Warden.test_mode!
     def associated_post(options = {})
    post_options = {
      title: 'Post title',
@@ -19,5 +20,15 @@ module TestFactories
   user 
  end
 
+ FactoryGirl.define do
+  factory :user do
+    email 'test@example.com'
+    password 'f4k3p455w0rd'
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    # if needed
+    # is_active true
+  end
+end
 
 end 
