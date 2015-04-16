9e27a83c66179bca279537eb761c96d0d816f458
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-04-16 13:04:42 +0400

    Removed debugging statements in posts controller

--- app/controllers/topics/posts_controller.rb
+++ app/controllers/topics/posts_controller.rb
@@ -25,14 +25,12 @@ class Topics::PostsController < ApplicationController
     @post = current_user.posts.build(post_params)
 
     @post.topic = @topic
-    puts @post.inspect
     authorize @post
     if @post.save
       @post.create_vote 
       flash[:notice] = "Post was saved"
       redirect_to [@topic, @post]
     else
-     puts @post.inspect
       flash[:error] = "There was an error saving the post. Please try again"
       render :new
     end
