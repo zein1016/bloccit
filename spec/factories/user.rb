4a480d0b1bb34e589b024f904bd23a148baaf5d0
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-05-06 18:45:12 +0400

    finished assignment

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,15 +1,17 @@
 FactoryGirl.define do
-   factory :user do
-     name "Douglas Adams"
-     sequence(:email, 100) { |n| "person#{n}@example.com" }
-     password "helloworld"
-     password_confirmation "helloworld"
-     confirmed_at Time.now
-    
+  factory :user do
+    name "Douglas Adams"
+    sequence(:email, 100) { |n| "person#{n}@example.com" }
+    password "helloworld"
+    password_confirmation "helloworld"
+    confirmed_at Time.now
+
     factory :user_with_post_and_comment do  
-     after(:build) do |user|
-      post
-      comment 
+      after(:build) do |user|
+        #build_list(:post, user.posts
+         # :comment, user.comments)
+        create(:post, user: user)
+        create(:comment , user: user)
       end
     end
   end
