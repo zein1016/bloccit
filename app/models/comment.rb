ada64bab6f1bceeeb14c9f411d31ec6f30a0268c
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-05-20 11:42:58 +0300

    finished creating with ajax assignment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
-  default_scope { order('created_at DESC')}
+  default_scope { order('created_at DESC')}.reverse
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
 
